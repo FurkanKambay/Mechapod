ab559995fb0989141a6b3ae5ec7604b3c2d56a48
Furkan Kambay <furkankambay@protonmail.com>
2025-07-13 16:30:20 +0300

    game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Shovel
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        [Header("References")]
+        [SerializeField] private MinionMouse minionMouse;
+
+        [Header("State")]
+        [SerializeField, Min(1)] private int dayNumber = 1;
+        [SerializeField] private bool isNight;
+
+        public int  DayNumber => dayNumber;
+        public bool IsNight   => isNight;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            dayNumber = 1;
+            isNight   = false;
+        }
+
+        [ContextMenu("Next Phase (Day/Night)")]
+        public void NextPhase()
+        {
+            if (isNight)
+                dayNumber++;
+
+            isNight = !isNight;
+
+            Time.timeScale = isNight ? 1f : 0f;
+        }
+    }
+}
